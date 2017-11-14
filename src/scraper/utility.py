71b88651ca8b51f17b7ab0c9a5b13e0da545e938
Matt Lewis <domattthings@gmail.com>
2017-11-13 19:22:31 -0500

    Moved app functions into a seperate utility file

---
+++ src/scraper/utility.py
@@ -0,0 +1,27 @@
+"""
+Quick little utility functions.
+
+Most of them interact with the provided redis client
+"""
+
+import json
+from nlp import process_txt
+
+# Quick Utility functions
+def append_nlp(result):
+    """Runs the nlp pipeline and adds to result"""
+    combined_txt = "\n".join([result['title'], result['desc'], result['story']])
+
+    return {**result, **process_txt(combined_txt)}
+
+def set_story(db_client, info):
+    """Adds the story to redis"""
+    info = json.dumps(info)
+    url = info['url']
+    db_client.pipeline().set(url, info).expire(url, 3600000).execute()
+
+    return
+
+def have_story(db_client, url):
+    """Quick check to see if the story is already in redis"""
+    return db_client.exists(url)
