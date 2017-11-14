e02e84fd443c0c95cf6a49460e5be7428f82d664
Matt Lewis <domattthings@gmail.com>
2017-11-13 20:53:39 -0500

    Removed bad sites

--- src/scraper/sites.py
+++ src/scraper/sites.py
@@ -1,45 +1,46 @@
-# This is the configuration for all sites to be scraped.
-#
-# The scraper will only get the front page, we are only looking for trending news.
-#
-# The link_regex will be the regex to match all links on the home page for.
-#
-# The story_xpath is a bit more driver specific, but it's  a css selector to get the story text.
+"""
+This is the configuration for all sites to be scraped.
+
+The scraper will only get the front page, we are only looking for trending news.
+
+The link_regex will be the regex to match all links on the home page for.
+
+The story_xpath is a bit more driver specific, but it's  a css selector to get the story text.
+"""
 
 sites = {
     'cnn': {
         'url': 'https://www.cnn.com',
-        'link_regex': 'https?:\/\/(www.)?cnn.com\/20*',
+        'link_regex': r'https?:\/\/(www.)?cnn.com\/20*',
         'story_xpath': '.zn-body__paragraph'
     },
     'bbc': {
         'url': 'http://www.bbc.com/',
-        'link_regex': 'https?:\/\/www.bbc.com\/news\/*',
-        'story_xpath': '.articleBody'
+        'link_regex': r'https?:\/\/www.bbc.com\/news\/*',
+        'story_xpath': '.story-body__inner'
     },
     'nyTimes': {
         'url': 'https://www.nytimes.com/',
-        'link_regex': 'https?:\/\/www.nytimes.com\/20*',
+        'link_regex': r'https?:\/\/www.nytimes.com\/20*',
         'story_xpath': '.story-body-text'
     },
     'guardian': {
         'url': 'https://www.theguardian.com',
-        'link_regex': 'https?:\/\/www.theguardian.com\/us-news\/*',
+        'link_regex': r'https?:\/\/www.theguardian.com\/us-news\/*',
         'story_xpath': '.content__article-body'
-    },
-    'usaToday': {
-        'url': 'https://www.usatoday.com',
-        'link_regex': 'https?:\/\/www.usatoday.com\/story\/*',
-        'story_xpath': '.p-text'
     },
     'eOnline': {
         'url': 'http://wwww.eonline.com',
-        'link_regex': 'https?:\/\/www.eonline.com\/news\/*',
+        'link_regex': r'https?:\/\/www.eonline.com\/news\/*',
         'story_xpath': '.post-content'
     },
-    'wallStreetJournal': {
-        'url': 'https://www.wsj.com',
-        'link_regex': 'https?:\/\/www.wsj.com\/articles\/*',
-        'story_xpath': '.wsj-snippet-body'
-    }
 }
+
+"""
+Ones that have been ruled out
+
++ NBC: infinite scroll
++ Wall Street Journal: paywall
++ USA Today: frequent pop ups
+
+"""
