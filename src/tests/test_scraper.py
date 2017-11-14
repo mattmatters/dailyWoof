340a638f3421562a95ca5a327d0cf1c60c4dbf48
Matt Lewis <domattthings@gmail.com>
2017-11-13 20:54:33 -0500

    Test all sites

--- src/tests/test_scraper.py
+++ src/tests/test_scraper.py
@@ -9,46 +9,68 @@ browser = webdriver.Remote(
 
 browser.implicitly_wait(2)
 
-# Tests that we can get a tags off the home page
-def test_cnn_scrape():
-    stories = scraper.get_links(browser, sites['cnn']['url'])
-    assert len(stories) > 0
-
 # Tests that our regex works
 def test_cnn_regex():
     stories = scraper.get_links(browser, sites['cnn']['url'])
+    assert len(stories) > 0
     stories = scraper.extract_links(stories, sites['cnn']['link_regex'])
     assert len(stories) > 0
 
-# Tests that we can get a tags off the home page
-def test_bbc_scrape():
-    stories = scraper.get_links(browser, sites['bbc']['url'])
-    assert len(stories) > 0
+def test_cnn_story():
+    story = scraper.get_story(browser, 'http://www.cnn.com/2017/11/13/politics/george-hw-bush-groping-allegation/index.html', sites['cnn']['story_xpath'])
+    assert len(story['title']) > 0
+    assert len(story['desc']) > 0
+    assert len(story['story']) > 0
 
 # Tests that our regex works
 def test_bbc_regex():
     stories = scraper.get_links(browser, sites['bbc']['url'])
+    assert len(stories) > 0
     stories = scraper.extract_links(stories, sites['bbc']['link_regex'])
     assert len(stories) > 0
 
-# Tests that we can get a tags off the home page
-def test_nyTimes_scrape():
-    stories = scraper.get_links(browser, sites['nyTimes']['url'])
-    assert len(stories) > 0
+def test_bbc_story():
+    story = scraper.get_story(browser, 'http://www.bbc.com/news/world-us-canada-41973952', sites['bbc']['story_xpath'])
+    assert len(story['title']) > 0
+    assert len(story['desc']) > 0
+    assert len(story['story']) > 0
 
 # Tests that our regex works
 def test_nyTimes_regex():
     stories = scraper.get_links(browser, sites['nyTimes']['url'])
+    assert len(stories) > 0
     stories = scraper.extract_links(stories, sites['nyTimes']['link_regex'])
     assert len(stories) > 0
 
-# Tests that we can get a tags off the home page
-def test_guardian_scrape():
-    stories = scraper.get_links(browser, sites['guardian']['url'])
-    assert len(stories) > 0
+def test_nyTimes_story():
+    story = scraper.get_story(browser, 'https://www.nytimes.com/2017/11/09/opinion/nuisance-ordinances-eviction-violence.html?action=click&pgtype=Homepage&clickSource=story-heading&module=opinion-c-col-left-region&region=opinion-c-col-left-region&WT.nav=opinion-c-col-left-region', sites['nyTimes']['story_xpath'])
+    assert len(story['title']) > 0
+    assert len(story['desc']) > 0
+    assert len(story['story']) > 0
 
 # Tests that our regex works
 def test_guardian_regex():
     stories = scraper.get_links(browser, sites['guardian']['url'])
+    assert len(stories) > 0
     stories = scraper.extract_links(stories, sites['guardian']['link_regex'])
     assert len(stories) > 0
+
+
+def test_guardian_story():
+    story = scraper.get_story(browser, 'https://www.theguardian.com/us-news/2017/nov/09/one-year-later-trump-takes-a-grand-tour-of-asia-as-clinton-visits-wisconsin-finally', sites['guardian']['story_xpath'])
+    assert len(story['title']) > 0
+    assert len(story['desc']) > 0
+    assert len(story['story']) > 0
+
+# Tests that our regex works
+def test_eOnline_regex():
+    stories = scraper.get_links(browser, sites['eOnline']['url'])
+    assert len(stories) > 0
+    stories = scraper.extract_links(stories, sites['eOnline']['link_regex'])
+    assert len(stories) > 0
+
+def test_eOnline_story():
+    story = scraper.get_story(browser, 'http://www.eonline.com/news/893550/did-kylie-jenner-have-a-private-baby-shower-all-the-details-on-her-pink-filled-celebration', sites['eOnline']['story_xpath'])
+    assert len(story['title']) > 0
+    assert len(story['desc']) > 0
+    assert len(story['story']) > 0
