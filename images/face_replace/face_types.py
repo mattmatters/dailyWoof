0c67a67940af8e1561b7d6d4b1eb0709ece79bf6
Matt Lewis <domattthings@gmail.com>
2017-12-15 19:50:15 -0500

    Migrate face_replace to external repository

--- images/face_replace/face_types.py
+++
@@ -1,8 +0,0 @@
-"""Hide the magic numbers"""
-FACE_TYPES = {
-    'FRONT': 1,
-    'FRONT_LEFT': 2,
-    'FRONT_RIGHT': 3,
-    'LEFT': 4,
-    'RIGHT': 5
-}
--- images/test.py
+++ images/test.py
@@ -1,15 +1,10 @@
-from face_replace.replacer import ReplaceFace
+import requests
+from face_replace import replace_faces
 
-Replacer = ReplaceFace("./paths/haarcascade_frontalface_alt.xml",
-                       "./paths/haarcascade_profileface.xml")
+FILE_PATH = './image.jpg'
+URL = 'https://static01.nyt.com/images/2017/12/12/us/12Alabama1/12Alabama1-facebookJumbo.jpg'
 
-newIm = Replacer.replace_faces("./test_images/thing2.jpg", "./dmx-head.png")
+img = requests.get(URL).content
+
+newIm = replace_faces(img, "./dmx-head.png")
 newIm.save('./test_good/new.png')
-newIm = Replacer.replace_faces("./test_images/thing3.jpg", "./dmx-head.png")
-newIm.save('./test_good/new2.png')
-newIm = Replacer.replace_faces("./test_images/thing4.jpg", "./dmx-head.png")
-newIm.save('./test_good/new3.png')
-newIm = Replacer.replace_faces("./test_images/thing5.jpg", "./dmx-head.png")
-newIm.save('./test_good/new4.png')
-newIm = Replacer.replace_faces("./test_images/thing6.jpg", "./dmx-head.png")
-newIm.save('./test_good/new5.png')
