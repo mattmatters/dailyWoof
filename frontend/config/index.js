0d1ae5b89f5f77793b0919241cd79677099d685f
Matt Lewis <domattthings@gmail.com>
2018-03-02 16:27:36 -0500

    Production cdn path

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -5,13 +5,15 @@
 
 const path = require('path')
 
+const publicPath = process.env.BUILD === 'PROD' ? 'https://cdn.dailywoof.space/' : '/';
+
 module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: publicPath,
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
