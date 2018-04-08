ea241a7d54311c53ed3abc6cd326ac3aca47e805
Matt Lewis <domattthings@gmail.com>
2018-04-07 20:31:33 -0400

    Refactor natlang

--- natlang/app.py
+++ natlang/app.py
@@ -3,6 +3,7 @@ Natural Language Processor
 
 Receives scraped news and identifies common nouns, adjectives, and proper nouns
 """
+import os
 import re
 import json
 import logging
@@ -10,21 +11,16 @@ import time
 import pika
 from natlang.nlp import process_txt
 
-# Set up logging
-WORKER_INFO = {'clientip': '298', 'user': 'nlp'}
-FORMAT = '%(asctime)-15s %(message)s'
-logging.basicConfig(format=FORMAT)
-LOGGER = logging.getLogger('nlp_worker')
-
-QUEUE_NAME = 'text'
-NEXT_QUEUE_NAME = 'images'
-CONNECTION_PARAMETERS = pika.ConnectionParameters(
-    'rabbitmq-service', retry_delay=5, connection_attempts=20)
-CONNECTION = pika.BlockingConnection(CONNECTION_PARAMETERS)
-CHANNEL = CONNECTION.channel()
-
 START_TIME = time.time()
 
+MQ_HOST = os.environ['MQ_HOST'] or 'rabbitmq-service'
+MQ_PORT = os.environ['MQ_PORT'] or 5672
+QUEUE_NAME = 'text'
+NEXT_QUEUE_NAME = 'images'
+MESSAGE_PROPERTIES = pika.BasicProperties(content_type='application/json')
+CONNECTION_PARAMETERS = pika.ConnectionParameters(MQ_HOST,
+                                                  retry_delay=5,
+                                                  connection_attempts=20)
 
 def append_nlp(result):
     """Runs the nlp pipeline and adds to result"""
@@ -42,29 +38,36 @@ def extract_name(url):
 def callback(ch, method, properties, body):
     """RabbitMQ callback"""
     body = json.loads(body.decode('utf-8'))
-    LOGGER.info('Processing: %s', body['url'], extra=WORKER_INFO)
+    logging.info('Processing: %s', body['url'])
     body = append_nlp(body)
 
-    ch.basic_publish(exchange='', routing_key=NEXT_QUEUE_NAME, body=json.dumps(body))
+    ch.basic_publish(exchange='',
+                     routing_key=NEXT_QUEUE_NAME,
+                     body=json.dumps(body),
+                     properties=MESSAGE_PROPERTIES)
 
     # Should sub this out with a more graceful solution
     if time.time() - START_TIME > 3600:
         exit()
 
+def setup_mq():
+    connection = pika.BlockingConnection(CONNECTION_PARAMETERS)
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='stories', exchange_type='fanout')
+    channel.queue_declare(queue=NEXT_QUEUE_NAME)
+    queue = channel.queue_declare(queue=QUEUE_NAME)
+
+    channel.queue_bind(exchange='stories', queue=queue.method.queue)
+    channel.basic_qos(prefetch_count=1)
+    return channel
+
 
 def main():
     """Subscribe to queue and story processed stories"""
-    # Create a queue and bind it to the stories exchange
-    CHANNEL.exchange_declare(exchange='stories', exchange_type='fanout')
-    CHANNEL.queue_declare(queue=NEXT_QUEUE_NAME)
-    queue = CHANNEL.queue_declare(queue=QUEUE_NAME)
-    CHANNEL.queue_bind(exchange='stories', queue=queue.method.queue)
-
-    # Start consuming
-    CHANNEL.basic_qos(prefetch_count=1)
-    CHANNEL.basic_consume(callback, queue=QUEUE_NAME, no_ack=True)
-    CHANNEL.start_consuming()
-
+    channel = setup_mq()
+    channel.basic_consume(callback, queue=QUEUE_NAME, no_ack=True)
+    channel.start_consuming()
 
 if __name__ == '__main__':
     main()
